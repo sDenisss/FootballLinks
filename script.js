07f25815d28d9f0fd5253b821c8256b52da47cee
sDenisss <ddenis22072006@gmail.com>
2025-08-13 16:48:42 +0600

    add crv - lech

---
+++ script.js
@@ -0,0 +1,23 @@
+const modal = document.getElementById("videoModal");
+const videoFrame = document.getElementById("videoFrame");
+const closeModal = document.getElementById("closeModal");
+
+document.querySelectorAll(".match-card").forEach(card => {
+    card.addEventListener("click", () => {
+        const videoURL = card.getAttribute("data-video");
+        videoFrame.src = videoURL;
+        modal.style.display = "block";
+    });
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    videoFrame.src = ""; // Очищаем, чтобы видео остановилось
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+        videoFrame.src = "";
+    }
+});
